d2377a639beab32aca9fa50e91e46e080a3ee2ef
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-05-22 12:45:19 +0300

    Добавьте файлы проекта.

---
+++ EFPractices/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EFPractices
+{
+    public class AppContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLExpress02;Database=EF;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True;");
+        }
+    }
+}
+
---
+++ EFPractices/Program.cs
@@ -0,0 +1,19 @@
+﻿using EFPractices;
+using AppContext = EFPractices.AppContext;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        using (var db = new AppContext())
+        {
+            var user1  = new User { Name = "Arthur", Role = "Admin" };
+            var user2 = new User { Name = "Klim", Role = "User" };
+
+            db.Users.Add(user1);
+            db.Users.Add(user2);
+            db.SaveChanges();
+        }
+    }
+}
